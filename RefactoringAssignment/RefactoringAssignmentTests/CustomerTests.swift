c518a2180f86ce4c6d0071c3dd907b5a49305ff4
Ahmed Ramy <dev.ahmedramy@icloud.com>
2020-01-30 23:33:13 +0200

    Write insurance test for `Customer.receipt()`

--- RefactoringAssignment/RefactoringAssignmentTests/CustomerTests.swift
+++ RefactoringAssignment/RefactoringAssignmentTests/CustomerTests.swift
@@ -7,27 +7,45 @@
 //
 
 import XCTest
+@testable import RefactoringAssignment
 
 class CustomerTests: XCTestCase {
 
+    var sut: Customer!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = Customer(name: "Ahmed")
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testReceiptReturnsGivenStringInAssignment() {
+        // Given
+        sut.addFamilyRide(ride: Ride(service: Service.uberBlack, kilometers: 300, time: 150, tolls: [5,4,6], isSurged: true, surgeRate: 1.5))
+        sut.addFamilyRide(ride: Ride(service: Service.uberX, kilometers: 200, time: 66, tolls: [5,6,6], isSurged: false, surgeRate: 0))
+        sut.addFamilyRide(ride: Ride(service: Service.chopper, kilometers: 160, time: 55, tolls: [5,4,6], isSurged: false, surgeRate: 0))
+        sut.addFamilyRide(ride: Ride(service: Service.uberBlack, kilometers: 3, time: 22, tolls: [5,6,6], isSurged: true, surgeRate: 1.4))
+        sut.addFamilyRide(ride: Ride(service: Service.uberX, kilometers: 200, time: 90, tolls: [5,4,6], isSurged: false, surgeRate: 0))
+        sut.addFamilyRide(ride: Ride(service: Service.chopper, kilometers: 140, time: 130, tolls: [5,6,6], isSurged: true, surgeRate: 1.3))
+        
+        // When
+        let returnedString = sut.receipt()
+        
+        // Then
+        let expectedString = """
+Receipt for:Ahmed
+LE 23755.50
+LE 6717.00
+LE 11050.00
+LE 3427.00
+LE 9115.00
+LE 26050.00
+Amount owed is LE 80114.50, and 0.00 point\n
+"""
+        
+        XCTAssert(expectedString == returnedString)
     }
 
 }
